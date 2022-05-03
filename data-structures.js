7bc3c316fe9114b40fe505a8d376eb66e603d2dc
binod164 <binod7_grg@hotmail.com>
2022-05-03 11:13:08 -0500

    Add  push and pop method in singly linked list

---
+++ data-structures.js
@@ -0,0 +1,50 @@
+//Singly Linked List
+
+class Node {
+  constructor(val){
+      this.val = val;
+      this.next = null;
+  }
+}
+
+class SinglyLinkedList{
+  costructor(){
+      this.head=null;
+      this.tail=null;
+      this.length = 0;
+      
+  }
+  push(val){
+      let newNode = new Node(val);
+      if(!this.head){
+          this.head = newNode;
+          this.tail = this.head;
+      }else{
+          this.tail.next = newNode;
+          this.tail = newNode;
+      }
+      this.length++;
+      return this;
+  }
+  pop(){
+      if(!this.head){
+          return undefined;
+      }else{
+          let current = this.head;
+          let newTail = current;
+          while(current.next){
+              newTail = current;
+              current = current.next;  
+          }
+          this.tail = newTail;
+          this.tail.next = null;
+          this.length--;
+          if(this.length === 0){
+              this.head = null;
+              this.tail= null;
+          }
+          return current;
+          
+      }
+  }
+}
